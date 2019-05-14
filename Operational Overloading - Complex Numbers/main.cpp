5d549af3f974a47007e4054685c2748264402791
Haider Ali Punjabi <haiderali@cyberservices.com>
2019-05-14 11:45:03 +0530

    From PC

--- Operational Overloading - Complex Numbers/main.cpp
+++ Operational Overloading - Complex Numbers/main.cpp
@@ -5,6 +5,7 @@
  * Date: 9/5/2019
  */
 #include <iostream>
+#include <curses.h>
 
 using namespace std;
 
@@ -39,11 +40,52 @@ public:
 
 int main()
 {
-    ComplexNumber c1 = ComplexNumber(4,-2);
-    ComplexNumber c2 = ComplexNumber(8,10);
-    ComplexNumber c3 = c1 + c2;
-    c1.PrintNumber();
-    c2.PrintNumber();
-    c3.PrintNumber();
+    int choice, numReal1, numImg1, numReal2,numImg2;
+    ComplexNumber c1 = ComplexNumber();
+    ComplexNumber c2 = ComplexNumber();
+    ComplexNumber c3 = ComplexNumber();
+
+    while(1){
+        system("CLS");
+        cout << "\t\tMain Menu"<<endl;
+        cout << "1. Add Complex Numbers\n2. Add Complex Number & Real Number\n0. Exit" <<endl;
+        cin >> choice;
+        switch(choice){
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            system("CLS");
+            cout << "Enter Real Part of Number 1: ";
+            cin >> numReal1;
+            cout << "Enter Imaginary Part of Number 1: ";
+            cin >> numImg1;
+            cout << "Enter Real Part of Number 2: ";
+            cin >> numReal2;
+            cout << "Enter Imaginary Part of Number 2: ";
+            cin >> numImg2;
+            c1.SetValues(numReal1,numImg1);
+            c2.SetValues(numReal2,numImg2);
+            c3 = c1 + c2;
+            cout << "Sum of Numbers: ";
+            c3.PrintNumber();
+            getch();
+            break;
+        case 2:
+            system("CLS");        
+            cout << "Enter Real Part of Number 1: ";
+            cin >> numReal1;
+            cout << "Enter Imaginary Part of Number 1: ";
+            cin >> numImg1;
+            cout << "Enter Real Number 2: ";
+            cin >> numReal2;
+            c1.SetValues(numReal1,numImg1);
+            c2 = c1 + numReal2;
+            cout << "Sum of Numbers: ";
+            c2.PrintNumber();
+            getch();
+            break;
+        }
+    }
     return 0;
 }
